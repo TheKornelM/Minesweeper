29745777087f1bfef19ec60d034839cd964e879d
TheKornelM <makari.kornel@gmail.com>
2025-07-26 09:10:02 +0200

    Fix upper-case mistype in a filename

--- src/menu/MenuHandler.h
+++ src/menu/MenuHandler.h
@@ -3,9 +3,9 @@
 
 #include <Adafruit_FT6206.h>
 #include <Adafruit_ILI9341.h>
-#include <FieldDisplay.h>
 #include <Minesweeper.h>
 
+#include "../FieldDisplay.h"
 #include "BaseMenuPage.h"
 #include "BoardSizeMenu.h"
 #include "MainMenuButtonWithMessagePage.h"
